3ac5d1540e5257470516e39030e26cbc20109ebc
gjn1231 <88516901+gjn1231@users.noreply.github.com>
2022-03-12 10:23:40 +0800

    测试

---
+++ T2_16.cpp
@@ -0,0 +1,20 @@
+/*
+filename:2_16.cpp
+author:gjn
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cout << "input a: ";
+    cin >> a;
+    cout << "input b: ";
+    cin >> b;
+
+    cout << "a+b=" << a+b << "\na*b=" << a*b << "\na-b=" << a-b << "\na/b=" << a/b;
+
+    return 0;
+}
